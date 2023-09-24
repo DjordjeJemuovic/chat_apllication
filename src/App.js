65d4950359ab02aadc1bee6ada4d2daedab73d47
DjordjeJemuovic <jemuovicdjordje98@gmail.com>
2023-09-24 13:11:01 +0200

    login comit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import {ChatEngine} from 'react-chat-engine';
 import './App.css';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
+const projectID = '192d2808-3d77-486b-b88e-5f5e0e77c02a';
 
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+    
     return (
         <ChatEngine
         height="100vh"
-        projectID="192d2808-3d77-486b-b88e-5f5e0e77c02a"
-        userName="Djordje Master"
-        userSecret="123456"
+        projectID={projectID}
+        userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         
         />
